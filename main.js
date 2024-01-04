f20aa4dac66ac65c4fedc1baec6705591bc5bee4
Tayla Phillips <richardstayla@gmail.com>
2024-01-04 14:26:09 -0600

    Add clickables class to delete icon

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ var currentShift = 0;
 function storeIdea(event) {
     var html = `<div class="card">
     <div class="delete-box" id="${Date.now()}">
-        <img class="delete-button" src="assets/delete.svg" alt="delete button">
+        <img class="delete-button clickables" src="assets/delete.svg" alt="delete button">
     </div>
     <h2 class="card-title">${titleField.value}</h2>
     <p class="card-body">${bodyField.value}</p>
