4e81855080163f3225bb3d5a221444d05363d03a
Tayla Phillips <richardstayla@gmail.com>
2024-01-05 17:44:00 -0600

    Attempt newfound bug fix

--- main.js
+++ main.js
@@ -1,17 +1,21 @@
-var titleField = document.querySelector("#submitted-title");
-var bodyField = document.querySelector("#submitted-body");
+var titleField = document.querySelector(".submitted-title");
+var bodyField = document.querySelector(".submitted-body");
 var saveButton = document.querySelector(".save-button");
 var ideaGrid = document.querySelector("#card-display");
 var nextArrow = document.querySelector("#next-arrow");
 var backArrow = document.querySelector("#back-arrow");
 var filterButton = document.querySelector("#filter");
 
+
 saveButton.addEventListener("click", function (event) {
   event.preventDefault();
   handleSave();
 });
+
 nextArrow.addEventListener("click", nextIdea);
+
 backArrow.addEventListener("click", prevIdea);
+
 ideaGrid.addEventListener("click", function (event) {
   if (event.target.className.includes("delete-button")) {
     var cardId = event.target.parentElement.id;
@@ -26,9 +30,13 @@ ideaGrid.addEventListener("click", function (event) {
 });
 
 titleField.addEventListener("input", checkFields);
+
 bodyField.addEventListener("input", checkFields);
-filterButton.addEventListener("click", filterIdeas);
+
+filterButton.addEventListener("click", handleFilterButton);
+
 document.addEventListener("click", checkStars);
+
 
 var ideas = [];
 var currentShift = 0;
@@ -36,6 +44,7 @@ var favorites = [];
 var filter = false;
 
 saveButton.disabled = true;
+
 
 function checkFields() {
   if (titleField.value && bodyField.value) {
@@ -134,7 +143,7 @@ function prevIdea() {
 
 function deleteCard(iD) {
   iD = Number(iD);
-  for (i = 0; i < ideas.length; i++) {
+  for (var i = 0; i < ideas.length; i++) {
     if (ideas[i].id === iD) {
       ideas.splice(i, 1);
       if (ideas.length > 2){
@@ -143,9 +152,13 @@ function deleteCard(iD) {
       displayIdeas();
     }
   }
-  for (i = 0; i < favorites.length; i++) {
+  for (var i = 0; i < favorites.length; i++) {
     if (favorites[i].id === iD) {
       favorites.splice(i, 1);
+      if (filter && favorites.length === 0){
+        filter = false;
+        displayIdeas();
+      }
     }
   }
 }
@@ -168,30 +181,30 @@ function updateFavs(cardId) {
 }
 
 function toggleStar(starId) {
-//   console.log(starId);
   var starIcon = document.getElementById(starId);
-  // console.log(`clicked star element`, starIcon)
-  // console.log(starIcon.src)
-  // console.log(`defult icon displayed`, starIcon.src.includes("assets/star.svg"))
   if (starIcon.src.includes("assets/star.svg")) {
     starIcon.src = "assets/star-active.svg";
   } else if (starIcon.src.includes("assets/star-active.svg")) {
     starIcon.src = "assets/star.svg";
   }
-  // console.log(starIcon.src)
   if (filter && favorites.length === 0){
-    filter = false;
+    // filter = false;
+    filterIdeas();
   }
   displayIdeas();
 }
 
 function filterIdeas() {
-  filter = !filter; //  im so proud of this it will now toggle false and true hahah
+  filter = !filter;
   if (filter) {
     filterButton.innerHTML = "Show All Ideas";
   } else {
     filterButton.innerHTML = "Show Starred Ideas";
   }
+}
+
+function handleFilterButton() {
+  filterIdeas();
   displayIdeas();
 }
 
